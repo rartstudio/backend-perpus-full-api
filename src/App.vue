651ae0d8149ad0bf9a2956bcc59374d5e7ada0ed
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-08-20 23:59:39 +0700

    mempersiapkan halaman login, app bar dan link untuk ke login form, tinggal lanjut card, template sudah siap

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app dark class="app-bar" color="#1976D2">
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <router-link to="/" class="home__link">
+        <v-toolbar-title>Perpus GKKB PTK</v-toolbar-title>
+      </router-link>
+    </v-app-bar>
+    <v-navigation-drawer absolute temporary v-model="drawer">
+      <v-list nav>
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+        <router-link  to="/login" text class="sidebar__link">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-arrow-right</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>
+              Login
+            </v-list-item-title>
+          </v-list-item>
+        </router-link>
+          
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main class="main-content">
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+  
+    
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.app-bar {
+  font-family: 'Nunito', sans-serif;
 }
 
-#nav {
-  padding: 30px;
+.home__link {
+  color: white !important;
+  text-decoration: none;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.sidebar__link {
+  text-decoration: none;
+  color: #616161 !important;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.main-content {
+  margin-top:2rem;
 }
 </style>
+
+<script>
+export default {
+  name: "App",
+
+  components: {
+  },
+
+  data: () => ({
+    drawer: false,
+    group: '',
+    showPassword: false
+  })
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-card class="mt-10 mx-4">
+        <v-card-title class="mb-4 justify-center">
+            <h1 class="header__login">Member Login</h1>
+        </v-card-title>
+        <v-card-text class="mt-4">
+            <v-form>
+                <v-text-field
+                    prepend-icon="mdi-account"
+                    label="Username"
+                    outlined
+                />
+                <v-text-field 
+                    outlined
+                    label="Password" 
+                    :type="showPassword ? 'text' : 'password'" 
+                    prepend-icon="mdi-lock"
+                    :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+                    @click:append="showPassword = !showPassword"
+                />
+            </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+            <v-btn color="#1976D2" dark large class="ml-2">
+                <v-icon left>mdi-pencil</v-icon>
+                    Sign Up
+                </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="#42A5F5" dark large outlined class="mr-2">
+                <v-icon left>mdi-arrow-right</v-icon>
+                    Login
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+export default {
+  name: "Login",
+
+  components: {
+  },
+
+  data: () => ({
+    showPassword: false
+  })
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+.header__login {
+    color: #42A5F5;
+    text-align: center;
+    font-size: 1.5rem;
+}
+
+</style>
