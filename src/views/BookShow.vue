2b27aab36915e2a298f1bb454e8494d4bb334ac6
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-09-11 23:13:51 +0700

    mengubah tampilan bookshow

--- src/views/BookShow.vue
+++ src/views/BookShow.vue
@@ -1,42 +1,69 @@
 <template>
-    <div class="book d-flex flex-column justify-center align-center mt-4">    
-        <img class="book__img elevation-4" :src="link(book.data.cover)" height="200px" width="140px"> 
-        <h4 class="book__title mt-4">{{book.data.title}}</h4>
-        <p class="text-caption mt-2">{{book.data.author.name}}</p>
-        <div class="d-flex align-center review__book pa-1 mt-n2">
-            <v-icon color="#fff" size="13px">
-                mdi-star
-            </v-icon>
-            <div class="ml-1 review__number">
-                {{ rating(book.data.reviews) }}
+    <div class="book">
+        <div class="d-flex justify-center align-start mt-4 pa-4">
+            <div>
+                <img class="book__img elevation-4" :src="link(book.data.cover)" height="200px" width="140px"> 
             </div>
-        </div>
-        <p class="book__description text-caption mt-4">
-            {{ book.data.description }}
-        </p>
-        <div class="align-self-start review mb-4">Review</div>
-        <div class="align-self-start d-flex" v-for="(review, index) in book.data.reviews" :key="index">
-            <div class="mr-2">
-                <v-avatar color="indigo" size="24" class="align-self-start">
-                    <span class="white--text headline"></span>
-                </v-avatar>
+            <div class="ml-4">
+                <h4 class="book__title mt-4">{{book.data.title}}</h4>
+                <p class="text-caption mt-2">{{book.data.author.name}}</p>
+                <div>
+                    <v-chip
+                    outlined
+                    class="text--white"
+                    color="#0a369d"
+                    label
+                    dark
+                    >
+                    <v-icon left>ri ri-check-line</v-icon>
+                    Tersedia
+                </v-chip>
             </div>
-            <div class="d-flex flex-column align-start">
-                <div class="d-flex flex-row">    
-                    <div v-for="(star,index) in rates" :key="index">
-                        <v-icon color="#ebd229" size="13px">
-                            mdi-star
-                        </v-icon>
+            <!-- <div class="d-flex align-center review__book pa-1 mt-n2">
+                <v-icon color="#fff" size="13px">
+                    mdi-star
+                </v-icon>
+                <div class="ml-1 review__number">
+                    {{ rating(book.data.reviews) }}
+                </div>
+            </div> -->
+            
+            <!-- <div class="align-self-start review mb-4">Review</div> -->
+            <!-- <div class="align-self-start d-flex" v-for="(review, index) in book.data.reviews" :key="index">
+                <div class="mr-2">
+                    <v-avatar color="indigo" size="24" class="align-self-start">
+                        <span class="white--text headline"></span>
+                    </v-avatar>
+                </div>
+                <div class="d-flex flex-column align-start">
+                    <div class="d-flex flex-row">    
+                        <div v-for="(star,index) in rates" :key="index">
+                            <v-icon color="#ebd229" size="13px">
+                                mdi-star
+                            </v-icon>
+                        </div>
+                    </div>
+                    <div class="text-body-2">{{review.user.name}}</div>
+                    <div class="text-caption review__comment">
+                        {{ review.comment }}
                     </div>
                 </div>
-                <div class="text-body-2">{{review.user.name}}</div>
-                <div class="text-caption review__comment">
-                    {{ review.comment }}
-                </div>
+            </div> -->
             </div>
         </div>
-
+        <div class="pl-4">
+            <p class="font-weight-bold">Sinopsis</p>
+            <p class="book__description text-body-2 mt-2">
+                {{ book.data.description }}
+            </p>
+        </div>
+        <v-footer fixed class="book__footer">
+            <v-btn class="book__btn" dark color="#ca0b64">
+                <v-icon>ri ri-add-line</v-icon>
+                Keranjang</v-btn>
+        </v-footer>
     </div>
+    
 </template>
 
 <script>
@@ -95,13 +122,22 @@ export default {
 
 <style lang="scss" scoped>
     .book {
+        color: #0a369d;
+
+        &__footer {
+            background: none !important;
+        }
+
+        &__btn {
+            margin: 0 auto;
+        }
 
         &__description {
             color: #818181;
         }
     }
     .review {
-        color: #0a369d;
+        
 
         &__comment {
             color: #818181;
